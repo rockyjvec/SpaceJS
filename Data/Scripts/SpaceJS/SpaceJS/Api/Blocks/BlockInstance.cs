a3e8b52e21c1632e8fa2f6d2866a1e826de465e5
Andrew Ewing <drew@phenocode.com>
2019-12-29 21:22:00 -0700

    Added the getProperty method to BlockInstance so scripts can read properties from blocks

--- Data/Scripts/SpaceJS/SpaceJS/Api/Blocks/BlockInstance.cs
+++ Data/Scripts/SpaceJS/SpaceJS/Api/Blocks/BlockInstance.cs
@@ -19,10 +19,12 @@ namespace SpaceJS.Api.Blocks
     {
         private SpaceJS.Block js;
         private IMyTerminalBlock tb;
+        private Engine engine;
 
         private BlockInstance(Jint.Engine engine, IMyTerminalBlock tb) : base(engine, "Block")
         {
             this.tb = tb;
+            this.engine = engine;
         }
 
         public static BlockInstance CreateObject(Jint.Engine engine, IMyTerminalBlock tb)
@@ -39,6 +41,7 @@ namespace SpaceJS.Api.Blocks
             this.js = js;
 
             FastAddProperty("applyAction", new ClrFunctionInstance(Engine, "applyAction", ApplyAction), true, false, true);
+            FastAddProperty("getProperty", new ClrFunctionInstance(Engine, "getProperty", GetProperty), true, false, true);
         }
 
         public JsValue ApplyAction(JsValue obj, JsValue[] arguments)
@@ -55,6 +58,48 @@ namespace SpaceJS.Api.Blocks
 
             action.Apply(tb);
             return true;
+        }
+
+        public JsValue GetProperty(JsValue obj, JsValue[] arguments)
+        {
+            if (arguments.Length < 1)
+            {
+                return new JsNull();
+            }
+
+            var id = TypeConverter.ToString(arguments.At(0));
+
+            ITerminalProperty termProp = tb.GetProperty(id);
+            if (termProp == null)
+            {
+                List<ITerminalProperty> properties = new List<ITerminalProperty>();
+                tb.GetProperties(properties);
+                js.AppendCustomInfo("Warning! Invalid property:\n[" + tb.CustomName + "]." + id + ".\n\nValid properties:\n");
+
+                foreach (var property in properties)
+                {
+                    js.AppendCustomInfo("\t" + property.Id + "\n");
+                }
+                return new JsNull();
+            }
+            
+            if (termProp.TypeName == "Color")
+            {
+                var color = TerminalPropertyExtensions.GetValueColor(tb, id);
+                return new JsString($"[{color.R},{color.G},{color.B},{color.A}]");
+            } else if (termProp.TypeName == "Object") {
+                return JsValue.FromObject(this.engine, TerminalPropertyExtensions.GetValue<Object>(tb, id));
+            } else if (termProp.TypeName == "Boolean") {
+                return new JsBoolean(TerminalPropertyExtensions.GetValueBool(tb, id));
+            } else if (termProp.TypeName == "StringBuilder") {
+                return new JsString(TerminalPropertyExtensions.GetValue<StringBuilder>(tb, id).ToString());
+            } else if (termProp.TypeName == "Single") {
+                return new JsNumber(TerminalPropertyExtensions.GetValueFloat(tb, id));
+            } else
+            {
+                js.AppendCustomInfo("ERROR! Unsupported type. Contact the developer. [" + tb.CustomName + "].getProperty(" + id + "): (" + termProp.TypeName + ")\n");
+                return new JsNull();
+            }
         }
     }
 }
